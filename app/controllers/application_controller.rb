94dde7941ce29e1cca0c08df354289db1ea62656
Ankur Patel <apate169@uncc.edu>
2017-10-12 01:41:04 +0000

    Done with section 4.8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,3 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
 
-def show
-  id = params[:id]
-  @movie - Movie.find(id)
-  
-end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,10 @@ class MoviesController < ApplicationController
      # default: render 'new' template
   end 
   
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
    def create
        params.require(:movie)
         params[:movie].permit(:title,:rating,:release_date)
@@ -27,6 +31,20 @@ class MoviesController < ApplicationController
         flash[:notice] = "#{@movie.title} was successfully created."
         redirect_to movies_path
    end
+   
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(movie_params)  # new way  
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+    
+    def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      flash[:notice] = "Movie '#{@movie.title}' deleted."
+      redirect_to movies_path
+    end
   
 end
         
